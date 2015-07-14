718e5402d5f9570b63bc5b95f1b32b61f304650a
Joshua Sloane <joshuadsloane@gmail.com>
2015-07-14 16:18:50 -0400

    Added microcontroller project (nothing yet implimented).

---
+++ Microcontroller/AutomaTilesMain/AutomaTilesMain/AutomaTilesMain.c
@@ -0,0 +1,17 @@
+/*
+ * AutomaTilesMain.c
+ *
+ * Created: 7/14/2015 15:53:13
+ *  Author: Joshua
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
