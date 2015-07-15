3c104fcd91c3255e1de29cb5e170fe9a45a538ce
Joshua Sloane <joshuadsloane@gmail.com>
2015-07-15 15:59:24 -0400

    ISRs set up and AD interrupt handling brought over from test file.

--- Microcontroller/AutomaTilesMain/AutomaTilesMain/AutomaTilesMain.c
+++ Microcontroller/AutomaTilesMain/AutomaTilesMain/AutomaTilesMain.c
@@ -13,10 +13,50 @@
 #include "Pins.h"
 #include "Inits.h"
 
+volatile uint16_t holdoff = 10;//for temporarily preventing click outputs
+volatile uint8_t click = 0;//becomes non-zero when a click is detected
 int main(void)
 {
     while(1)
     {
         //TODO:: Please write your application code 
     }
+}
+
+ISR(INT0_vect){//INT0 interrupt triggered when the pushbutton is pressed
+	
+}
+
+ISR(PCINT0_vect){//Pin Change 0 interrupt triggered when any of the phototransistors change level
+	
+}
+
+//Values saved for derivative calculation
+volatile uint8_t adcOld = 0;
+volatile uint8_t adcNew = 0;
+volatile uint16_t medDelta = 1;
+ISR(ADC_vect){
+	adcNew = ADCH;// Record ADC value
+	sei(); //re-enable interrupts, allow this calculation to be interrupted
+	
+	int delta;
+	if(adcNew > adcOld){// Calculate delta
+		delta = adcNew-adcOld;
+		}else{
+		delta = adcOld-adcNew;
+	}
+	
+	adcOld = adcNew;// update old value
+	if(holdoff == 0){//holdoff can be set in main to disable click being set for a period of time
+		if(medDelta < delta){//check for click as the median is scaled up by 16, and exceptional event is needed.
+			click = delta;
+		}
+		}else{
+		holdoff--;
+	}
+	if((delta<<4)<medDelta){// update running median. Error on high side. note that due to comparison, the median is scaled up by 16
+		medDelta--;
+		}else{
+		medDelta++;
+	}
 }
--- Microcontroller/AutomaTilesMain/AutomaTilesMain/Inits.c
+++ Microcontroller/AutomaTilesMain/AutomaTilesMain/Inits.c
@@ -8,11 +8,14 @@
 #include "Inits.h"
 #include "Pins.h"
 
-void initIO(){//Set up pin directions, pull up/downs, overrides
+void initIO(){//Set up pin directions, pull up/downs, overrides, pin change interrupts
 	DDRA = IR; //Set IR LED pin as output
 	PORTA = 0x00; //No pull ups and IR LED is low
 	DDRB = LEDCLK|LEDDAT; //Set LED signals as outputs
 	PORTB = BUTTON; //Apply pull up to button, reset is pulled up by default
+	MCUCR|=(1<<ISC01)|(0<<ISC00);//Button interrupt is triggered on a falling edge
+	PCMSK0 = 0x3F; //mask out only the phototransistors for the pin change interrupt
+	GIMSK = (1<<INT0)|(0<<PCIE1)|(1<<PCIE0);//Enable INT0 interrupt for the button and Pin change interrupts for the phototransistors
 }
 
 void initAD(){//Set up all the necessary parameters for analog to digital conversion
@@ -25,8 +28,17 @@ void initAD(){//Set up all the necessary parameters for analog to digital conver
 	ADCSRB = (0 << BIN) // single ended mode
 		| (1 << ADLAR) // left align result to easily read only 8 MSB
 		| (0 << ADTS2) | (0 << ADTS1) | (0 << ADTS0); // auto trigger off of A/D interrupt (free running mode)
+	// start free running operation
+	ADCSRA |= (1 << ADSC);
 }
 
 void initTimer(){//Set up global .1ms timer used for various protocols
-	
+	TCCR0A = (0<<COM0A1)|(0<<COM0A0)//OC0A Disconnected
+		|(0<<COM0B1)|(0<<COM0B0)//OC0B Disconnected
+		|(1<<WGM01)|(0<<WGM00);//Clear timer on compare match
+	TCCR0B = (0<<WGM02)
+		|(0<<CS02)|(1<<CS01)|(0<<CS00);//ClkIO/8 (1MHz)
+	OCR0A = 100; //100 cycles at 1MHZ = .1ms
+	TIMSK0 = (0<<OCIE0B)|(1<<OCIE0A)|(0<<TOIE0);//Only enable OC Match A interrupt
 }
+
