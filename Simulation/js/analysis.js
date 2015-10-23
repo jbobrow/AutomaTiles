6eeb3cdbb1314a897434032d770143cdec43485c
Jonathan Bobrow <jbobrow@gmail.com>
2015-10-23 16:35:40 -0400

    adding a file to start implementing arrangement analysis in JS

---
+++ Simulation/js/analysis.js
@@ -0,0 +1,21 @@
+/**
+ * Created by jonathanbobrow on 10/19/15.
+ */
+
+// look at all present tiles
+// create an array of all combinations of starting points for the tiles 2^n
+// init tiles at each starting point
+// step forward until all off, board repeats its state for 2 times, recording its period
+    // i.e. a clover has a period of 1
+    // i.e. a blinker has a period of 2...
+// count # of ways that the end state is dead (all off)
+// count # of ways that the end state is static equilibrium
+// count # of ways that the end state is dynamic equilibrium
+
+// report properties of arrangment
+    // surface area (num on perimeter)
+    // # of 6 neighbor, 5 neighbor, 4, 3, 2, 1, 0
+        // yields density or sparseness
+    //
+
+// find bounding box of an arrangment
