f720907259014e68195a2572d6537366bfc3b98a
Jonathan Bobrow <jbobrow@gmail.com>
2016-03-19 03:20:32 -0400

    updated forest fire sketch to grow more trees when around trees and have a longer more animated lightning animation

--- CodeSketches/Visualizations/forestFires.c
+++ CodeSketches/Visualizations/forestFires.c
@@ -1,40 +1,41 @@
-/*	Forest Fire (Simulation/Visualization)
- *	
- *	Summary: each tile simulates a single tree and lives happily unless
- *	         struck by lightning or catching a spark of a neighboring fire
+/*   Forest Fire (Simulation/Visualization)
+ *   
+ *   Summary: each tile simulates a single tree and lives happily unless
+ *            struck by lightning or catching a spark of a neighboring fire
  *
- *	Rules: Based on Nicky Case's Simulating the World in Emoji
+ *   Rules: Based on Nicky Case's Simulating the World in Emoji
  *
- *	Dirt: % chance turning into a tree (10% in this case) to simulate growth
- *	      Display dim color (emberlike)
- *	Tree: If neighbor is on fire, % chance turning into fire
- *	      Display green     
- *	Fire: % chance turning into dirt (50% in this case) to simulate burning out (running out of fuel)
- *	      Display red/orange
- *	Lightning: Occur on button press, turn tree to fire
- *	      Display a flash or flashes of bright white
+ *   Dirt: % chance turning into a tree (10% in this case) to simulate growth
+ *         Display dim color (emberlike)
+ *   Tree: If neighbor is on fire, % chance turning into fire
+ *         Display green     
+ *   Fire: % chance turning into dirt (50% in this case) to simulate burning out (running out of fuel)
+ *         Display red/orange
+ *   Lightning: Occur on button press, turn tree to fire
+ *         Display a flash or flashes of bright white
  *
- *	by Jonathan Bobrow
- *	01.2016
+ *   by Jonathan Bobrow
+ *   01.2016
  */
 
 #include "AutomaTiles.h"
 
 uint8_t neighbors[6];
-uint8_t colors[4][3] = {{51,26,0},         // Dead Tree
+uint8_t colors[4][3] = {{8,4,0},         // Dead Tree
                          {26,255,0},       // Alive Tree
-                         {255,153,26},     // Fire
+                         {255,128,8},     // Fire
                          {204,204,255}};   // Lightning
+uint8_t darkColor[3] = {0,0,0};
 
 uint8_t bLightning = 0;
-uint16_t lightningDelay = 500;
+uint16_t lightningDelay = 1500;
 uint32_t lightningStrikeTime = 0;
+uint32_t currentTime = 0;
 
 void button() {
      // cause lighting
      bLightning = 1;
-     lightningStrikeTime = getTimer();
-     setColor(colors[3]);
+     lightningStrikeTime = currentTime;
 }
 
 void step() {
@@ -43,9 +44,17 @@ void step() {
      
      switch(getState()) {
           case 0: 
-               // we are dirt, become a tree occasionally 10%
-               if(getTimer()%10==0)
+               // we are dirt, become a tree occasionally 
+               // higher probability with other trees around
+               for(uint8_t i=0; i<6; i++) {
+                    if(neighbors[i] == 1 && currentTime % 6 == i) {
+                         setState(1);
+                    }
+               }
+               // if no trees around, 10%
+               if(currentTime % 10 == 0) {
                     setState(1);
+               }
                break;
           case 1: 
                for(uint8_t i=0; i<6; i++) {
@@ -56,7 +65,7 @@ void step() {
                }
                break;
           case 2: 
-               if(getTimer()%2==0)
+               if(currentTime % 2 ==0)
                     setState(0);   // burn out with 50% probability
                break;
           default: break;
@@ -76,16 +85,28 @@ int main(void) {
 
      // set initial state
      setState(1);
-
-     // Keep the processor running “
-     while(1){
+      
+      // listen to step forward
+     setMicOn();
+      
+     // Keep the processor running
+     while(1) {
+            currentTime = getTimer();
           // handle any continuous logic here
-          if(bLightning && getTimer() - lightningStrikeTime >= lightningDelay) {
-               if(getState() != 0) {
-                    setState(2);   // make fire
+          if(bLightning) {
+               if(currentTime - lightningStrikeTime >= lightningDelay) {
+                    if(getState() != 0) {
+                         setState(2);   // make fire
+                    }
+                    setColor(colors[getState()]);
+                    bLightning = 0;
                }
-               setColor(colors[getState()]);
-               bLightning = 0;
+               else if(currentTime%3 > 0) {     // this flashes the light during lightning
+                    setColor(darkColor);
+               }
+               else {
+                    setColor(colors[3]);
+               }
           }
      }
 }
