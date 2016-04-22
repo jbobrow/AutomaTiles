b62d27dbb35ef77850864dbf5427e56d19416987
Jonathan Bobrow <jbobrow@gmail.com>
2016-04-22 15:51:33 -0400

    adding music visualizer idea to work in progress

---
+++ Examples/_WorkInProgress/MusicVisualizer/MusicVisualizer.ino
@@ -0,0 +1,27 @@
+/*  Music Visualizer
+ *  
+ *  Setup: The tile will listen to its mic and determine the note based on
+ *  a simple FFT and will display the note is heard by lighting up in a specific color
+ *
+ *  Modes: (switched by pressing the button)
+ *  1) Color map all notes 
+ *  2) Respond to A ~ 440Hz
+ *  2) Respond to B ~ 440Hz
+ *  2) Respond to C ~ 440Hz
+ *  2) Respond to D ~ 440Hz
+ *  2) Respond to E ~ 440Hz
+ *  2) Respond to F ~ 440Hz
+ *
+ *  by Jonathan Bobrow
+ *  01.28.2016
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
