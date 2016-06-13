ab146651c892389e9f5400f155d5e5bd717eb51d
Jonathan Bobrow <jbobrow@gmail.com>
2016-06-13 17:02:20 -0400

    update fracture to stay brighter and to only change state after 10 confirmations of state change... a debounce for neighbor watching:

--- Examples/Fracture/Fracture.ino
+++ Examples/Fracture/Fracture.ino
@@ -32,7 +32,10 @@ uint32_t updateFrequency = 50;  //milliseconds
 uint8_t diversityValue;
 uint8_t possibleDiversityValue;
 
+uint32_t lastTimeColorChanged = 0;
+
 uint8_t neighbors[6];
+uint8_t prevNeighbors[6] = {255,255,255,255,255,255};
 
 uint8_t colors[6][3] = {{255,0,0},          // Red
                          {0,255,0},         // Green
@@ -47,16 +50,32 @@ uint8_t darkColor[3] = {0,0,0};
 // the color we will actually show
 uint8_t outputColor[3];
 
+// brightness
+uint8_t brightness[7] = {128,149,170,191,212,233,255};
+
 // minimum number of neighbors present
 // to be happy at all (i.e. tiles with only 
 // one neighbor, feel left out)
 uint8_t minNeighbors = 2; 
 
+// only update the state if the neighbors are confirmed to change
+uint8_t didNeighborsChangeCount = 0;
+uint8_t didNeighborsChangeCountThreshold = 10;
+
+// blinking for happiness
+uint8_t isBlinking = 0;
+
 // this gets triggered when we press the button
 void button() {
   // update the color/type we are
   // increment our state value by one between the range of (1-6)
-  setState(getState()%6 + 1);
+  if(getTimer() - lastTimeColorChanged > 2000) {
+    setState(getState()%6 + 1);
+    didNeighborsChangeCount = 10; // make sure to update color and state
+    prevNeighbors[0] = 255; // make sure at least one neighbor is different
+    isBlinking = 0;
+  }
+  lastTimeColorChanged = getTimer();
 }
 
 void step() {
@@ -77,47 +96,70 @@ void loop() {
 
     getNeighborStates(neighbors);
 
-    // get diversity
-    diversityValue = 0;
-    possibleDiversityValue = 0;
+    // check if we have a different neighbor
     for(uint8_t i=0; i<6; i++) {
-      // check if we have a neighbor
-      if(neighbors[i] != 0) {
-        possibleDiversityValue++;
-        // now we have a neighbor check its state
-        if(neighbors[i] != getState()) {
-          diversityValue++;
-        }
+      if(neighbors[i] != prevNeighbors[i]) {
+        didNeighborsChangeCount++;
+        break;
+      }
+
+      if(i==5) {
+        didNeighborsChangeCount = 0;
       }
     }
 
-    // happiness integer (0-6)
-    uint8_t happiness = possibleDiversityValue == 0 ? 6 : (diversityValue * 6) / (possibleDiversityValue); 
-    if(possibleDiversityValue < minNeighbors)
-      happiness = 0;
+    // if the surroundings changed
+    if(didNeighborsChangeCount >= 10) {
 
-    // sadness is the inverse of happiness
-    uint8_t sadness = 6 - happiness;
-
-    // now make the light bright based on how happy we are
-    // (or dim it based on how sad we are...)
-    outputColor[0] = colors[getState()-1][0] >> sadness;  // Red output value
-    outputColor[1] = colors[getState()-1][1] >> sadness;  // Green output value
-    outputColor[2] = colors[getState()-1][2] >> sadness;  // Blue output value
-    /*  NOTE: BITSHIFT OPERATOR
-     *  >>n operator simply divides the value by 2^n
-     *  (i.e. 128 >> 2 = 32 (128/2^2) -> (128/4) -> 32)
-     *  super handy for quick division or multiplication by powers of 2
-     */
+      // reset count
+      didNeighborsChangeCount = 0;
+      
+      // update prev neighbors
+      for(uint8_t i=0; i<6; i++) {
+        prevNeighbors[i] = neighbors[i];
+      }
+      
+      // get diversity
+      diversityValue = 0;
+      possibleDiversityValue = 0;
+      for(uint8_t i=0; i<6; i++) {
+        // check if we have a neighbor
+        if(neighbors[i] != 0) {
+          possibleDiversityValue++;
+          // now we have a neighbor check its state
+          if(neighbors[i] != getState()) {
+            diversityValue++;
+          }
+        }
+      }
     
-    // to help players know when the brightest value is showing
-    // blink if in happiest state or simply display the output color
-    if(happiness == 6 &&(getTimer() % 500) > 250 ) {
+      // happiness integer (0-6)
+      uint8_t happiness = possibleDiversityValue == 0 ? 6 : (diversityValue * 6) / (possibleDiversityValue); 
+      if(possibleDiversityValue < minNeighbors)
+        happiness = 0;
+    
+      // now make the light bright based on how happy we are
+      outputColor[0] = int(colors[getState()-1][0] / 255.0 * brightness[happiness]);  // Red output value
+      outputColor[1] = int(colors[getState()-1][1] / 255.0 * brightness[happiness]);  // Green output value
+      outputColor[2] = int(colors[getState()-1][2] / 255.0 * brightness[happiness]);  // Blue output value
+      
+      // to help players know when the brightest value is showing
+      // blink if in happiest state or simply display the output color
+      if(happiness == 6) {
+        isBlinking = 1;
+      }
+      else {
+        isBlinking = 0;
+      }    
+    }
+
+    if(isBlinking && (getTimer() % 500) > 250) {
         setColor(darkColor);
     }else {
         setColor(outputColor);  
     }
 
+    
     // update our update time
     lastUpdateTime = getTimer();
   }
