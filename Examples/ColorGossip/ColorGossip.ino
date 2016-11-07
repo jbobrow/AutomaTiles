823f3b0311c4f8f200dd106701f6f03d9fa27fe9
Jonathan Bobrow <jbobrow@gmail.com>
2016-11-07 12:52:55 -0500

    updated color gossip to show color changed to during press when alone

--- Examples/ColorGossip/ColorGossip.ino
+++ Examples/ColorGossip/ColorGossip.ino
@@ -26,24 +26,24 @@ uint8_t colors[7][3] = {{204,0,0},        // Red
                         {0,204,0}};       // Green
 
 uint8_t darkColor[3] = {0,0,0};
-uint8_t brightness[60] = {
-  64,71,77,84,90,96,102,107,
-  112,116,119,122,125,127,128,128,
-  128,127,125,122,119,116,112,107,
-  102,96,90,84,77,71,64,57,
-  51,44,38,32,26,21,16,12,
-  9,6,3,1,0,0,0,1,
-  3,6,9,12,16,21,26,32,
-  38,44,51,57};
+uint8_t brightness[30] = {
+  71,84,96,107,
+  116,122,127,128,
+  127,122,116,107,
+  96,84,71,57,
+  44,32,21,12,
+  6,1,0,1,
+  6,12,21,32,
+  44,57};
 
 uint8_t displayColor[3];
 
 uint8_t neighbors[6];
 
 uint32_t lastUpdateTime = 0;
-uint32_t updateFrequency = 50;  //milliseconds
+uint16_t updateFrequency = 50;  //milliseconds
 
-uint32_t waitTimeForStateChange = 5000;
+uint16_t waitTimeForStateChange = 5000;
 uint32_t timeSinceLastStateChange = 0;
 uint8_t curState = 1;
 uint8_t prevState = 1;
@@ -66,7 +66,7 @@ void loop() {
     uint8_t didChangeState = 0;
     if(aloneCount < 254)
       aloneCount++;
-    for(int i=0; i<6; i++) {
+    for(uint8_t i=0; i<6; i++) {
       if(neighbors[i] != 0) {
         aloneCount = 0;
         if(curTime - timeSinceLastStateChange > waitTimeForStateChange) {
@@ -83,12 +83,18 @@ void loop() {
     }
     
     if(aloneCount > 10) {// isAlone) {
-      //pulse white
-      int8_t idx = (curTime%3000)/50;
-      displayColor[0] = 32 + brightness[idx]; 
-      displayColor[1] = 32 + brightness[idx]; 
-      displayColor[2] = 32 + brightness[idx];
-      setColor(displayColor); 
+      //if just pressed
+      if(curTime - timeSinceLastStateChange < 500) {
+        setColor(colors[curState - 1]);
+      }
+      else {
+        //pulse white
+        int8_t idx = (curTime%3000)/100;
+        displayColor[0] = 32 + brightness[idx]; 
+        displayColor[1] = 32 + brightness[idx]; 
+        displayColor[2] = 32 + brightness[idx];
+        setColor(displayColor); 
+      }
     }
     else {
       // if need to change, change, otherwise, display color of state
